f019e5e8b9d05a284f04b2e95c1e1e07f370e5ed
CodeStrikerMayank <mayankbhatt9tha3@gmail.com>
2026-03-29 03:17:57 +0530

    addons

--- backprocess.c
+++ backprocess.c
@@ -5,7 +5,7 @@
 #include "operation.h"
 #include <windows.h>
 void display(struct Student s1[], int size){ 
-    int st;
+    int st;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
     printf("Id no");
     scanf("%d",&st);
     system("cls");
@@ -46,4 +46,50 @@ void infoboard(struct Student s1[],struct marks s2[],int size){
     if (st < 0 || st >= size) {
         printf(red "Error: Invalid ID number. Please enter an ID within the valid range.\n" reset);
     }
+}
+void wtboard(struct Student s1[],struct marks s2[],int size,int arr[]){
+    int len = sizeof(arr)/sizeof(arr[0]);
+    printf("==========================================================================\n");
+    for(int i =0;i<len;i++){
+        int avg = (s2[i].eng+s2[i].maths+s2[i].hindi+s2[i].science+s2[i].sst)/5;
+        printf("| %-3s | %-20s | %-7s | %-6s | %-4s | %-8s | %-6s | %-5s |\n",
+        "ID", "STUDENT NAME", "ENGLISH", "HINDI", "SST", "SCIENCE", "MATHS", "AVG");
+        printf("| %-3d | %-20s | %-7d | %-6d | %-4d | %-8d | %-6d | %-5.2f |\n",s1[arr[i]].id, s1[arr[i]].name, s2[arr[i]].eng, s2[arr[i]].hindi, s2[arr[i]].sst, s2[arr[i]].science, s2[arr[i]].maths, avg);}
+    printf("==========================================================================\n");}
+void sortersearch(struct Student s1[],struct  marks s2[],int size){int num;
+    while(1){
+    int arr[10];
+    system("cls");
+    printf(green);
+    printf("========================\n");
+    printf("------Details search----");
+    printf("|  1.Failed student    |\n");
+    printf("|  2.Average students  |\n");
+    printf("|  3.High scores       |\n");
+    printf("|  4.Deep search       |\n");
+    printf("|  5.CMD Touch         |\n");
+    printf("|  6.Exit              |\n");
+    printf("========================\n",reset);
+    printf("Enter your choice --> ");
+    scanf("%d",&num);
+    if (num==1){
+        for (int i = 0;i<=size;i++){
+            int k = 0;
+            int avg = (s2[i].eng+s2[i].maths+s2[i].hindi+s2[i].science+s2[i].sst)/5;
+            if (avg<33){
+                arr[k] = i;
+                k++;
+            }}
+            wtboard(s1,s2,size,arr);
+    }
+    else if (num ==2 ){}
+    else if (num==3){}
+    else if (num ==4){}
+    else if (num == 5){    }
+    else if (num == 6){ }
+    else {
+    printf(red,"Caution use available inputs !");
+    }
+
+}
 }
